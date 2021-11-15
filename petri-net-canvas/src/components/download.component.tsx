1fb2cb3c8f6750a02444e543eb2fc2b3352e2217
Dominik Mathar <d.mathar@t-online.de>
2021-11-15 19:17:07 +0100

    refactores file save to service

--- petri-net-canvas/src/components/download.component.tsx
+++ petri-net-canvas/src/components/download.component.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {PetriNet} from "../model/petri-net.interfaces";
-import FileSaver from "file-saver";
+import {FileSaveService} from "../services/file-save.service";
 
 class DownloadComponent extends Component<{ petriNet: PetriNet }, any> {
 
@@ -10,35 +10,7 @@ class DownloadComponent extends Component<{ petriNet: PetriNet }, any> {
 
     onClick() {
         const net = this.props.petriNet;
-        console.log('places')
-        net.places.forEach(place => console.log(place));
-        console.log('transitions')
-        net.transitions.forEach(transition => console.log(transition));
-        console.log('arcs')
-        net.arcs.forEach(arc => console.log(arc))
-        const file = this.buildFile(net);
-        FileSaver.saveAs(file);
-    }
-
-    private buildFile(net: PetriNet) {
-        const fileContent = ['.type pn \n']
-        fileContent.push('.transitions \n');
-        net.transitions.forEach(transition => {
-            fileContent.push(transition.exportName);
-            fileContent.push('\n');
-        });
-        fileContent.push('.places \n');
-        net.places.forEach(place => {
-            fileContent.push(place.exportName);
-            fileContent.push(' ' + place.marks.length);
-            fileContent.push('\n');
-        });
-        fileContent.push('.arcs \n');
-        net.arcs.forEach(arc => {
-            fileContent.push(arc.start.exportName + ' ' + arc.end.exportName);
-            fileContent.push('\n');
-        });
-        return new File(fileContent, "pn.txt", {type: "text/plain;charset=utf-8"});
+        FileSaveService.saveFile(net);
     }
 
     render() {
