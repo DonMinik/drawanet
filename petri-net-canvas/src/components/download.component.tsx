a77f3845658c40754e69b07ffda271ec5bb45dbe
Dominik Mathar <d.mathar@t-online.de>
2021-11-15 19:23:56 +0100

    removes lint error

--- petri-net-canvas/src/components/download.component.tsx
+++ petri-net-canvas/src/components/download.component.tsx
@@ -4,10 +4,6 @@ import {FileSaveService} from "../services/file-save.service";
 
 class DownloadComponent extends Component<{ petriNet: PetriNet }, any> {
 
-    constructor(props: { petriNet: PetriNet }) {
-        super(props);
-    }
-
     onClick() {
         const net = this.props.petriNet;
         FileSaveService.saveFile(net);
