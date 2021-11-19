9c60eaca366475a20f3cad6a9ab0195acf0bcc86
Dominik Mathar <d.mathar@t-online.de>
2021-11-19 22:54:55 +0100

    removes log statements

--- src/services/scale.service.ts
+++ src/services/scale.service.ts
@@ -7,10 +7,8 @@ export class ScaleService {
     static scale(scale: number):number {
         if (!scaleStore.initialized) {
             scaleStore.initialized = true;
-            console.log('set Scale', scale)
             scaleStore.scale = scale;
         }
-        console.log(scaleStore.scale);
         return scaleStore.scale;
     }
 }
