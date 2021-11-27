0b857bfd791d14970ea2fb15cb974b953345f025
Dominik Mathar <d.mathar@t-online.de>
2021-11-27 12:50:53 +0100

    experimenting with tesseract js version 2

--- src/services/text-detection.service.ts
+++ src/services/text-detection.service.ts
@@ -2,20 +2,27 @@ import Tesseract from "tesseract.js";
 
 export class TextDetectionService {
 
-    private static readonly TESSERACT_CONFIG = {
-        logger: m => console.log(m),
-      //Todo: this does not work, but maybe try to find config parameters to increase precision
-        // tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    }
+    private static readonly TESSERACT_CONFIG_WHITELIST =  'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-    static async detectText(canvas: HTMLCanvasElement ): Promise<any> {
+    static async detectText(canvas: HTMLCanvasElement ): Promise<string> {
         const ctx = canvas.getContext('2d');
         const src =  ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const result = await  Tesseract.recognize(src,'eng', this.TESSERACT_CONFIG)
-            .catch (err => {
-                console.error(err);
+
+        const worker = Tesseract.createWorker();
+        let result: string;
+        await (async () => {
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            await worker.setParameters({
+                tessedit_char_whitelist: this.TESSERACT_CONFIG_WHITELIST,
             });
-        return result.text?.trim()
+            const {data: {text}} = await worker.recognize(src);
+            console.log(text);
+            result = text?.trim();
+            await worker.terminate();
+        })();
+        return result;
     }
 }
 
