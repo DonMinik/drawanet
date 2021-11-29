11d63a86b49e1ad8f8e97dfa85747d9d461e685d
Dominik Mathar <d.mathar@t-online.de>
2021-11-29 13:43:35 +0100

    adds another help text and fixes wrong grammar

--- src/components/help.component.tsx
+++ src/components/help.component.tsx
@@ -21,8 +21,9 @@ class HelpComponent extends Component<{ callBack: HelpCallback }, any> {
                         <p>Tokens can be added by clicking within a place</p>
                         <p>Start arc within a place or transition and end the line within another.</p>
                         <p>Use double click to name a place or transition and write the name into the black field. The close by clicking outside the field.</p>
-                        <p>Currently only capital Letters and Numbers is supported. If your writing is recognized incorrectly please try to write the character as close to printed characters as possible.</p>
+                        <p>Currently only capital letters and numbers is supported. If your writing is recognized incorrectly please try to write the character as close to printed characters as possible.</p>
                         <p>To draw arc weights draw the same arc twice or multiple times.</p>
+                        <p>Elements you draw are not movable. The size of places and transitions is defined by the first one you draw. Keep this in mind if you need space for a large net.</p>
                         <p>If you need to delete any element just cross out the element you wish to delete.(Tokens can be removed with a single click.) </p>
                 </div>
                 <div className='backdrop' onClick={() => this.onClick()}/>
