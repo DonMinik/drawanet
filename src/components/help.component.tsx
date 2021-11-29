1c9c35bc9a7657e0d8d0b106561695dacdaa3fee
Dominik Mathar <d.mathar@t-online.de>
2021-11-29 10:14:14 +0100

    adds help texts

--- src/components/help.component.tsx
+++ src/components/help.component.tsx
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
 
-class HelpComponent extends Component<{ callBack: HelpCallback}, any> {
+class HelpComponent extends Component<{ callBack: HelpCallback }, any> {
 
     private readonly callBack: HelpCallback;
 
-    constructor(props: { callBack: HelpCallback}) {
+    constructor(props: { callBack: HelpCallback }) {
         super(props);
         this.callBack = props.callBack;
     }
@@ -14,15 +14,17 @@ class HelpComponent extends Component<{ callBack: HelpCallback}, any> {
     }
 
     render() {
-        return(
-            <div >
+        return (
+            <div>
                 <div className='help'>
-                    Hold down left mouse button to draw places, transitions, arcs, and tokens. <br/>
-                    Use double click to name a place or transition.
+                        <p>Hold down left mouse button to draw places, transitions, arcs, and tokens.</p>
+                        <p>Tokens can be added by clicking within a place</p>
+                        <p>Start arc within a place or transition and end the line within another.</p>
+                        <p>Use double click to name a place or transition and write the name into the black field. The close by clicking outside the field.</p>
+                        <p>To draw arc weights draw the same arc twice or multiple times.</p>
+                        <p>If you need to delete any element just cross out the element you wish to delete.(Tokens can be removed with a single click.) </p>
                 </div>
-                <div className='backdrop' onClick={() => this.onClick()}>
-
-                </div>
+                <div className='backdrop' onClick={() => this.onClick()}/>
             </div>
         );
     }
